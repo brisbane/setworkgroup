44c0e45d7e28426b7a5cd7bdc9b1c464b6400a9d
kashif74 <>
2013-11-07 17:17:24 +0000

    first commit

---
+++ manifests/init.pp
@@ -0,0 +1,28 @@
+class setworkgroup ($factname, $value) {
+  tag("kickstart") 
+
+  ensure_resource ( 'file', '/etc/facter/', {
+    'ensure' => 'directory',
+    'mode'   => '0755',
+    'owner'  => 'root',
+    'group'  => 'root',
+    } )
+
+ ensure_resource ( 'file', '/etc/facter/facts.d/', {
+    'ensure' => 'directory',
+    'mode' => '0755',
+    'owner' => 'root',
+    'group' => 'root',
+   
+    } )
+ file { "/etc/facter/facts.d/${factname}.yaml" :
+     ensure  => present,
+     owner   => 'root',
+     group   => 'root',
+     mode    => '0644',
+     content => "${factname}: ${value}"
+
+} 
+ 
+
+}
